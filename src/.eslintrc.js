d6bdde89a9a7bdb4bd6e166d4974f0fc16976730
Kishan Nirghin <kishan@itslogical.nl>
2021-11-25 21:49:16 +0000

    feat: setup eslint

---
+++ src/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "jsx-a11y", "prettier"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        semi: false,
+      },
+    ],
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: [".tsx"],
+      },
+    ],
+  },
+}
